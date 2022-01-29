af2031237d8e05a537c9f45c0e12d0778f2817f9
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-01-29 13:13:38 -0500

    terminada del video

--- script.js
+++ script.js
@@ -18,9 +18,9 @@
 // ********* para abrir el spinner o loader *********
 ((d) => {
     const $form = d.querySelector(".contact-form"),
-        $loader = d.querySelector(".contac-form-loader"),
+        $loader = d.querySelector(".contact-form-loader"),
         $response = d.querySelector(".form-response");
-    $form.addEventListener("submit", (e) => {
+    $form.addEventListener("submit", e => {
         e.preventDefault();
         $loader.classList.remove("none");
         fetch("https://formsubmit.co/ajax/san.agustin20.aqz@gmail.com", {
@@ -33,10 +33,10 @@
             location.hash = "#gracias";
             $form.reset();
         })
-        .catch((er) => {
-            console.log(er);
-            let message = er.statusText || "Ocurrio un error, intente nuevamente.";
-            $response.querySelector("h3").innerHTML = `Error ${er.status}: ${message}`;
+        .catch((err) => {
+            console.log(err);
+            let message = err.statusText || "Ocurrio un error, intente nuevamente";
+            $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
         }).finally(() => {
             $loader.classList.add("none");
             setTimeout(() => {
