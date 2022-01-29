a29878de74fa3c5c509e2281f2f678afa419c6db
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-01-28 19:32:56 -0500

    hasta formulario de envio con error en el javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ********* menu *********
+// ********* para abrir el menu mobile *********
 ((d) => {
     const $btnmenu = d.querySelector(".menu-btn"),
         $menu = d.querySelector(".menu");
@@ -14,3 +14,35 @@
         });
     })
 })(document);
+
+// ********* para abrir el spinner o loader *********
+((d) => {
+    const $form = d.querySelector(".contact-form"),
+        $loader = d.querySelector(".contac-form-loader"),
+        $response = d.querySelector(".form-response");
+    $form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/san.agustin20.aqz@gmail.com", {
+            method:"POST",
+            body: new FormData(e.target),
+        })
+        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+        .then((json) => {
+            console.log(json);
+            location.hash = "#gracias";
+            $form.reset();
+        })
+        .catch((er) => {
+            console.log(er);
+            let message = er.statusText || "Ocurrio un error, intente nuevamente.";
+            $response.querySelector("h3").innerHTML = `Error ${er.status}: ${message}`;
+        }).finally(() => {
+            $loader.classList.add("none");
+            setTimeout(() => {
+               location.hash = "#close"; 
+            }, 3000);
+        });
+    })
+
+})(document)
