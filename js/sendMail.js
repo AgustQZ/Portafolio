a93f52e2f6ab6d921a706855ab3c9e4e30d2ddaa
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-04-07 23:21:47 -0500

    arreglo detalla en modulo enviar email y corrijo menu en modo escritorio

--- js/sendMail.js
+++ js/sendMail.js
@@ -32,38 +32,3 @@ export default function sendMail(form, loader, response) {
             });
     });
 }
-
-// ********* para enviar el correo *********
-/*((d) => {
-    const $form = d.querySelector(".contact-form"),
-        $loader = d.querySelector(".contact-form-loader"),
-        $response = d.querySelector(".form-response");
-    $form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/san.agustin20.aqz@gmail.com", {
-            method: "POST",
-            body: new FormData(e.target),
-        })
-            .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-            .then((json) => {
-                console.log(json);
-                location.hash = "#gracias";
-                $form.reset();
-            })
-            .catch((err) => {
-                console.log(err);
-                let message =
-                    err.statusText || "Ocurrio un error, intente nuevamente";
-                $response.querySelector(
-                    "h3"
-                ).innerHTML = `Error ${err.status}: ${message}`;
-            })
-            .finally(() => {
-                $loader.classList.add("none");
-                setTimeout(() => {
-                    location.hash = "#close";
-                }, 3000);
-            });
-    });
-})(document);*/
