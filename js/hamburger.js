20d60d1a9cb7ecdc50afc43030eda2e4a95dcab5
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-04-11 17:29:24 -0500

    toco fuera del menu para cerrarlo, funcionando.

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,13 +1,19 @@
-export default function hamburguerBtn(btn, menu, menuLink) {
+export default function hamburguerBtn(btn, menu, menuLink, falseBackgraund) {
     const d = document;
     d.querySelector(btn).addEventListener("click", (e) => {
-        d.querySelector(btn).firstElementChild.classList.toggle("none");
+        d.querySelector(falseBackgraund).classList.toggle("none");
         d.querySelector(menu).classList.toggle("activo");
 
         d.addEventListener("click", (e) => {
             if (!e.target.matches(menuLink)) return false;
-            d.querySelector(btn).firstElementChild.classList.remove("none");
+            d.querySelector(falseBackgraund).classList.add("none");
             d.querySelector(menu).classList.remove("activo");
         });
+    });
+    d.querySelector(falseBackgraund).addEventListener("click", (e) => {
+        if (e.target.matches(falseBackgraund)) {
+            d.querySelector(falseBackgraund).classList.add("none");
+            d.querySelector(menu).classList.toggle("activo");
+        }
     });
 }
--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ import sendMail from "./sendMail.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
-    hamburguerBtn(".menu-btn", ".menu", ".menu a");
+    hamburguerBtn(".menu-btn", ".menu", ".menu a", ".false-background");
     sendMail(".contact-form", ".contact-form-loader", ".form-response");
 });
