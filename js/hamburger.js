ecdbeb357435a0a568098784e71ed51effda7373
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-04-12 16:04:09 -0500

    pongo x a boton hamburguesa e informacion a imagenes de trabajos

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,30 @@
-export default function hamburguerBtn(btn, menu, menuLink, falseBackgraund) {
+export default function hamburguerBtn(
+    btn,
+    svg1,
+    svg2,
+    menu,
+    menuLink,
+    falseBackgraund
+) {
     const d = document;
     d.querySelector(btn).addEventListener("click", (e) => {
-        d.querySelector(falseBackgraund).classList.toggle("none");
+        d.querySelector(svg1).classList.toggle("none");
+        d.querySelector(svg2).classList.toggle("none");
         d.querySelector(menu).classList.toggle("activo");
+        d.querySelector(falseBackgraund).classList.toggle("none");
 
         d.addEventListener("click", (e) => {
             if (!e.target.matches(menuLink)) return false;
             d.querySelector(falseBackgraund).classList.add("none");
             d.querySelector(menu).classList.remove("activo");
+            d.querySelector(svg1).classList.toggle("none");
+            d.querySelector(svg2).classList.toggle("none");
         });
     });
     d.querySelector(falseBackgraund).addEventListener("click", (e) => {
-        if (e.target.matches(falseBackgraund)) {
-            d.querySelector(falseBackgraund).classList.add("none");
-            d.querySelector(menu).classList.toggle("activo");
-        }
+        d.querySelector(falseBackgraund).classList.add("none");
+        d.querySelector(menu).classList.toggle("activo");
+        d.querySelector(svg1).classList.toggle("none");
+        d.querySelector(svg2).classList.toggle("none");
     });
 }
--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ import sendMail from "./sendMail.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
-    hamburguerBtn(".menu-btn", ".menu", ".menu a", ".false-background");
+    hamburguerBtn(
+        ".menu-btn",
+        ".svg1",
+        ".svg2",
+        ".menu",
+        ".menu a",
+        ".false-background"
+    );
     sendMail(".contact-form", ".contact-form-loader", ".form-response");
 });
