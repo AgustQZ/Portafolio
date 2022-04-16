88d7a586d9eb775478c369fe535e431c4c233b15
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-04-16 09:09:17 -0500

    corrijo error de menu hamburguesa

--- js/hamburger.js
+++ js/hamburger.js
@@ -14,11 +14,12 @@ export default function hamburguerBtn(
         d.querySelector(falseBackgraund).classList.toggle("none");
 
         d.addEventListener("click", (e) => {
-            if (!e.target.matches(menuLink)) return false;
-            d.querySelector(falseBackgraund).classList.add("none");
-            d.querySelector(menu).classList.remove("activo");
-            d.querySelector(svg1).classList.toggle("none");
-            d.querySelector(svg2).classList.toggle("none");
+            if (e.target.matches(menuLink)) {
+                d.querySelector(falseBackgraund).classList.add("none");
+                d.querySelector(menu).classList.remove("activo");
+                d.querySelector(svg1).classList.remove("none");
+                d.querySelector(svg2).classList.add("none");
+            }
         });
     });
     d.querySelector(falseBackgraund).addEventListener("click", (e) => {
